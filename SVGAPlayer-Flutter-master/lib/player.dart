e404f3a4edb47008b773d7a5d5335b859edd75ed
Salma Elbasha <m.elgamal0551@gmail.com>
2025-02-28 02:15:50 +0200

    Update player.dart

--- SVGAPlayer-Flutter-master/lib/player.dart
+++ SVGAPlayer-Flutter-master/lib/player.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:svgaplayer_flutter/parser.dart';
+import 'package:flutter/rendering.dart';
 import 'package:svgaplayer_flutter/proto/svga.pb.dart';
+import 'package:svgaplayer_flutter/parser.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'audio_handler.dart';
+
+class SVGAImage extends StatefulWidget {
+  final SVGAAnimationController controller;
+
+  const SVGAImage(this.controller, {Key? key}) : super(key: key);
+
+  @override
+  _SVGAImageState createState() => _SVGAImageState();
+}
 
 class SVGAAnimationController extends AnimationController {
   MovieEntity? _videoItem;
   bool _isDisposed = false;
 
-  SVGAAnimationController({required TickerProvider vsync}) 
-      : super(vsync: vsync, duration: Duration.zero);
+  SVGAAnimationController({required TickerProvider vsync}) : super(vsync: vsync, duration: Duration.zero);
 
   set videoItem(MovieEntity? value) {
     if (_isDisposed) return;
@@ -25,10 +35,8 @@ class SVGAAnimationController extends AnimationController {
 
   MovieEntity? get videoItem => _videoItem;
 
-  void startAnimation() {
-    if (_videoItem != null) {
-      forward(from: 0.0);
-    }
+  void clear() {
+    if (!_isDisposed) notifyListeners();
   }
 
   @override
@@ -39,38 +47,14 @@ class SVGAAnimationController extends AnimationController {
   }
 }
 
-class SVGAImage extends StatefulWidget {
-  final SVGAAnimationController controller;
-  final String assetName;
-
-  const SVGAImage(this.controller, {Key? key, required this.assetName}) : super(key: key);
-
-  @override
-  _SVGAImageState createState() => _SVGAImageState();
-}
-
 class _SVGAImageState extends State<SVGAImage> {
   MovieEntity? video;
 
   @override
   void initState() {
     super.initState();
-    loadAnimation();
+    video = widget.controller.videoItem;
     widget.controller.addListener(_handleChange);
-  }
-
-  Future<void> loadAnimation() async {
-    final parser = SVGAParser(sharedContext);
-    parser.decodeFromAssets(widget.assetName, (movie) {
-      if (mounted) {
-        setState(() {
-          widget.controller.videoItem = movie;
-          widget.controller.startAnimation();
-        });
-      }
-    }, onError: (error) {
-      debugPrint("Error loading SVGA: $error");
-    });
   }
 
   void _handleChange() {
@@ -107,12 +91,7 @@ class _SVGAPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    if (controller.videoItem == null) return;
-    final videoItem = controller.videoItem!;
-    final currentFrame = (controller.value * videoItem.params.frames).toInt();
-    if (currentFrame >= videoItem.params.frames) return;
-    final frame = videoItem.sprites[currentFrame];
-    frame.draw(canvas, size);
+    // إضافة منطق الرسم هنا
   }
 
   @override
