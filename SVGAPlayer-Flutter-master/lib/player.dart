d81de20f49021f333049336f4113f435595ae976
Salma Elbasha <m.elgamal0551@gmail.com>
2025-03-01 17:41:12 +0200

    Update player.dart

--- SVGAPlayer-Flutter-master/lib/player.dart
+++ SVGAPlayer-Flutter-master/lib/player.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:svgaplayer_flutter/proto/svga.pb.dart';
 import 'package:svgaplayer_flutter/parser.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'audio_handler.dart';
 
 class SVGAImage extends StatefulWidget {
   final SVGAAnimationController controller;
@@ -18,26 +15,27 @@ class SVGAAnimationController extends AnimationController {
   MovieEntity? _videoItem;
   bool _isDisposed = false;
 
-  SVGAAnimationController({required TickerProvider vsync}) : super(vsync: vsync, duration: Duration.zero);
+  SVGAAnimationController({required TickerProvider vsync})
+      : super(vsync: vsync, duration: Duration.zero);
 
   set videoItem(MovieEntity? value) {
     if (_isDisposed) return;
     if (isAnimating) stop();
     _videoItem = value;
+
     if (value != null) {
       int fps = value.params.fps > 0 ? value.params.fps : 20;
       duration = Duration(milliseconds: (value.params.frames / fps * 1000).toInt());
+      reset();
+      forward(); // Ensure animation starts playing
     } else {
       duration = Duration.zero;
     }
-    reset();
+
+    notifyListeners(); // Important to trigger UI update
   }
 
   MovieEntity? get videoItem => _videoItem;
-
-  void clear() {
-    if (!_isDisposed) notifyListeners();
-  }
 
   @override
   void dispose() {
@@ -87,13 +85,60 @@ class _SVGAImageState extends State<SVGAImage> {
 class _SVGAPainter extends CustomPainter {
   final SVGAAnimationController controller;
 
-  _SVGAPainter(this.controller);
+  _SVGAPainter(this.controller) : super(repaint: controller);
 
   @override
   void paint(Canvas canvas, Size size) {
-    // إضافة منطق الرسم هنا
+    if (controller.videoItem == null) return;
+
+    final frameIndex = (controller.value * (controller.videoItem!.params.frames - 1)).toInt();
+    final frame = controller.videoItem!.sprites;
+
+    for (var sprite in frame) {
+      if (frameIndex < sprite.frames.length) {
+        final drawFrame = sprite.frames[frameIndex];
+        canvas.drawImage(drawFrame.bitmap, Offset.zero, Paint()); // Draw frame
+      }
+    }
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late SVGAAnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SVGAAnimationController(vsync: this);
+    _loadSVGAAnimation();
+  }
+
+  Future<void> _loadSVGAAnimation() async {
+    final parser = SVGAParser();
+    final videoItem = await parser.decodeFromAssets('assets/animation.svga');
+    _controller.videoItem = videoItem;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: SVGAImage(_controller),
+        ),
+      ),
+    );
+  }
 }
