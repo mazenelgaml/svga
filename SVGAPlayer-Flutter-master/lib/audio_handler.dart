aaa95d7ea031a2c27d2365009e745752c1ded982
Salma Elbasha <m.elgamal0551@gmail.com>
2025-03-02 02:11:21 +0200

    Update audio_handler.dart

--- SVGAPlayer-Flutter-master/lib/audio_handler.dart
+++ SVGAPlayer-Flutter-master/lib/audio_handler.dart
@@ -4,11 +4,21 @@ import 'package:path_provider/path_provider.dart';
 import 'package:svgaplayer_flutter/proto/svga.pb.dart';
 
 class AudioHandler {
-  final AudioPlayer _player = AudioPlayer();
+  final AudioPlayer _player = AudioPlayer();  // Singleton instance of AudioPlayer
   bool _isMuted = false;
   bool _isPlaying = false;
   File? _audioFile;
 
+  // Avoid recreating audio handler. It will be created once and disposed of later
+  AudioHandler._privateConstructor();
+  static final AudioHandler _instance = AudioHandler._privateConstructor();
+  
+  // Factory method to return the singleton instance
+  factory AudioHandler() {
+    return _instance;
+  }
+
+  // Play audio from the SVGA MovieEntity, called once per video load
   Future<void> playAudioFromSVGA(MovieEntity videoItem) async {
     if (videoItem.audios.isEmpty) return;
 
@@ -25,8 +35,11 @@ class AudioHandler {
       await _audioFile!.writeAsBytes(audioData);
     }
 
-    await _player.play(DeviceFileSource(_audioFile!.path));
-    _isPlaying = true;
+    // Only start playing if the audio is not already playing
+    if (!_isPlaying) {
+      await _player.play(DeviceFileSource(_audioFile!.path));
+      _isPlaying = true;
+    }
   }
 
   void muteAudio(bool mute) {
